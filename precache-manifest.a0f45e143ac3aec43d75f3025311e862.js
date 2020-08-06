5b33efe34acb2b0ce4368870219e095b89bfc085
unknown <fionaodoboh@gmail.com>
2020-08-06 23:35:16 +0100

    Updates

---
+++ precache-manifest.a0f45e143ac3aec43d75f3025311e862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156eb898953d86e27a557b0ed438acb5",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "2f890e1dc80831ab1b77",
+    "url": "/Todo-app/static/css/main.25ceb1b1.chunk.css"
+  },
+  {
+    "revision": "3fa6d5a02ddc0c58f0da",
+    "url": "/Todo-app/static/js/2.de96f8a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.de96f8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f890e1dc80831ab1b77",
+    "url": "/Todo-app/static/js/main.2431bf62.chunk.js"
+  },
+  {
+    "revision": "1eb6d6ec195e648776ea",
+    "url": "/Todo-app/static/js/runtime-main.375335ae.js"
+  },
+  {
+    "revision": "3b3542ed66975683a0ce26449b3fa5bf",
+    "url": "/Todo-app/static/media/bg-img.3b3542ed.jpeg"
+  }
+]);
